df0f9394cea3a151fa8d0aa5fcb16a293b3eaf18
pr12iya <120767379+pr12iya@users.noreply.github.com>
2022-12-20 10:57:02 +0530

    Create majority element.java

---
+++ majority element.java
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int ele=0;
+        for(int num:nums)
+        {
+            if(count==0)
+            {
+                ele=num;
+            }
+            if(num==ele)
+            {
+                count+=1;
+            }
+            else{
+                count-=1;
+            }
+        }
+        return count;
+    }
+};
